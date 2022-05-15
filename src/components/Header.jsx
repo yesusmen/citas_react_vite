5fcf46c9520f458162b2d2d94ad799c83c7efc8a
Jesus Araujo <yesusmen@gmail.com>
2022-05-15 18:29:08 -0400

    mi primer cambio en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header () {
     return (
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-                Seguimiento Pacientes {""}
+                Seguimiento Pacientes {""} con GitHub {''}
                     <span className="text-indigo-600">Venterinaria</span>
                 </h1>
         </>
